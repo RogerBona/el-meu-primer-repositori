ee843800b29bc81dc47d751a09d8cb4855ff13e2
Roger Bonaventura <rogerbonaventura@inspladelestany.cat>
2021-02-25 16:28:46 +0100

    inicialitzar un projecte

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+	
+	   public static void main (String[] args) {
+		   
+		   int num1 =10;
+		   int num2 =4;
+		   
+		   //guarda l edat
+		   int edat = num1 + num2;
+		   
+		   System.out.println("Hola noi, tens " + edat + " anys ");
+
+
+	   }
+	   
+	   
+}
