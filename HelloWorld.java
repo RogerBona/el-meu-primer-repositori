996dbd75aa92196698f6febd801946fc48340e62
Roger Bonaventura <rogerbonaventura@inspladelestany.cat>
2021-02-25 16:47:57 +0100

    projecta 2n part

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,8 @@
 public class HelloWorld {
 	
 	   public static void main (String[] args) {
-		   
-		   int num1 =10;
-		   int num2 =4;
-		   
-		   //guarda l edat
-		   int edat = num1 + num2;
-		   
-		   System.out.println("Hola noi, tens " + edat + " anys ");
+		   	   
+		   System.out.println("Hola David ");
 
 
 	   }
